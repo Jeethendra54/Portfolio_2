53c36fd6475353e21abc1fd44dde7fd5002e9f47
Jeethendra54 <shankarbadavath.jadav54@gmail.com>
2023-09-27 23:29:39 +0530

    changed bg image , added services section,started building popup

---
+++ js/services.js
@@ -0,0 +1,12 @@
+const tags = document.getElementsByClassName('service-popup');
+console.log(tags)
+// tag.array.forEach(element => {
+//     print(tag[element])
+// });
+
+for(let i=0;i<tags.length;i++){
+    let tag = tags[i]
+    tag.addEventListener('click',e=>{
+        
+    })
+}
